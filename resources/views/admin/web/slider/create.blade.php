c1e3146945fa2417d4ef9769cc58870876290abf
unimediaafrica <unimediaafrica@gmail.com>
2025-08-16 04:56:36 +0300

    Slider Button fixed

--- resources/views/admin/web/slider/create.blade.php
+++ resources/views/admin/web/slider/create.blade.php
@@ -53,7 +53,7 @@
 
                         <div class="form-group col-md-6">
                             <label for="button_link">{{ __('field_button_link') }}</label>
-                            <input type="url" class="form-control" name="button_link" id="button_link" value="{{ old('button_link') }}">
+                            <input type="text" class="form-control" name="button_link" id="button_link" value="{{ old('button_link') }}">
 
                             <div class="invalid-feedback">
                               {{ __('required_field') }} {{ __('field_button_link') }}
